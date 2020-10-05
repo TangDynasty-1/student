99f20635ff225777727247d588f6e5c2ec9e3ace
zsk <zsk@111.com>
2020-10-05 18:49:45 +0800

    在家开发的签到功能的其他功能50%

---
+++ SignIn.java
@@ -0,0 +1,11 @@
+public class SignIn{
+        public static void main(String[] args){
+
+                private String name;
+                private String signTime;
+
+                public void sign(){
+					System.out.println(signTime);
+				}
+        }
+}
